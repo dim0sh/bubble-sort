88dd399b748ed706d706eb5e8a37aec09b6def23
Iain Dorsch <i_dorsch21@informatik.uni-kl.de>
2024-01-05 22:49:14 +0100

    add selection sort fixed bubblesort index error inner loop

--- sorting.c
+++ sorting.c
@@ -14,7 +14,7 @@ int bubble_sort(int arr[],int n)
     for (i = 0; i<n-1; i++)
     {
         swapped = 0;
-        for (j = i; j<n-1; j++)
+        for (j = 0; j<n-i-1; j++)
         {
             if (arr[j] > arr[j+1])
             {
@@ -29,16 +29,37 @@ int bubble_sort(int arr[],int n)
     }
 }
 
+int selection_sort(int arr[],int n)
+{
+    int i,j;
+    for (i = 0; i<n-1; i++)
+    {
+        for (j = i+1; j<n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                swap(&arr[i],&arr[j]);
+            }
+        }
+    }
+}
+
 void print_arr(int * arr, int n)
 {
     for (int i = 0; i<n; i++)
     {
-        printf("%d\n",arr[i]);
+        printf("%d ",arr[i]);
     }
+    printf("\n");
 }
 
 int main() {
     int arr[] = { 1, 9, 8, 4, 3, 7, 10, 11, 2, 5 };
+    printf("bubble\n");
     bubble_sort(arr,10);
     print_arr(arr,10);
+    int arr2[] = { 1, 9, 8, 4, 3, 7, 10, 11, 2, 5 };
+    printf("selection\n");
+    selection_sort(arr2,10);
+    print_arr(arr2,10);
 }
